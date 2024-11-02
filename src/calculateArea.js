473ff2ca7362db22a42e27c87d057b9bc47e021e
vicmarchesi <victor.marchesi91@gmail.com>
2024-11-02 12:30:14 +0000

    Solved Lab

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,16 @@
-function calculateArea() {
-
+function calculateArea(length, width) {
+    if(length == undefined || width == undefined || typeof length !== "number" || typeof width !== "number") {
+        return undefined;
+    }
+    return length * width;
 }
+
+
+
+/* function divide (numOne, numTwo) {
+    if(numOne == undefined || numTwo == undefined || typeof numOne !== "number" || typeof numTwo !== "number") {
+        return undefined;
+    }
+    return numOne / numTwo;
+}
+*/
--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,29 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(calculateArea).toBeDefined();
+        });
+        it("should take two numbers as arguments", () => {
+            expect(calculateArea.length).toBe(2);
+          });
 
+        it("should return a number representing the area of a rectangle (the product of the two arguments).", () => {
+            expect(calculateArea(10, 2)).toEqual(20);
+            expect(calculateArea(15, 3)).toEqual(45);
+            expect(calculateArea(100, 50)).toEqual(5000);
+        
     })    
-})
-
-*/
+        it("should return undefined if any of the arguments is not provided", () => {
+        expect(calculateArea(1)).toEqual(undefined);
+        expect(calculateArea()).toEqual(undefined);
+        expect(calculateArea(undefined, 1)).toEqual(undefined);
+      });
+         it("should return undefined if any of the two arguments is not a number", () =>{
+        expect(calculateArea("12", 1)).toBeUndefined();
+        expect(calculateArea(1, "30")).toBeUndefined();
+        expect(calculateArea("string", "string")).toBeUndefined();
+      })
+    });
+});
